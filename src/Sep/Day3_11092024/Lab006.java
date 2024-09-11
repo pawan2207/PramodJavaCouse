cc7efa586223ddcd5cc7eded13380ec606ae208a
Pawan <pawan220787@gmail.com>
2024-09-11 22:04:41 +0530

    added formatted printing

---
+++ src/Sep/Day3_11092024/Lab006.java
@@ -0,0 +1,25 @@
+package Sep.Day3_11092024;
+
+public class Lab006 {
+    public static void main(String[] args)
+    {
+        int a = 15;
+        System.out.printf("Value of a is %d",a);
+        int age = 45;
+        System.out.printf("Your age is %d", age);
+        char a1 = '\n';
+        System.out.printf("%c",a1);
+        char my = 'a';
+        System.out.printf("your fav character is %c", my);
+        float f = 452.56f;
+        System.out.printf("your floating num is %f",f);
+
+
+
+
+
+
+
+
+    }
+}
