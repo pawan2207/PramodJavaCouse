e5be0ba0973d4d55fd51f2212a531ddc2cdb54d8
Pawan <pawan220787@gmail.com>
2024-09-11 13:00:53 +0530

    added 3day exercise

--- src/Sep/Day1_04092024/Lab004.java
+++ src/Sep/Day1_04092024/Lab004.java
@@ -1,4 +1,12 @@
 package Sep.Day1_04092024;
 
 public class Lab004 {
+    public static void main(String[] args)
+    {
+        boolean $ = true; //boolean can start with $ and _
+        System.out.println($);
+        boolean i_have_car = false;
+        boolean _abc = false;
+        System.out.println(_abc );
+    }
 }
---
+++ src/Sep/Day3_11092024/Lab005.java
@@ -0,0 +1,9 @@
+package Sep.Day3_11092024;
+
+public class Lab005 {
+    public static void main(String[] args)
+    {
+        int a = 473847;
+    //int is datatype. a is variable, 473847 is value. this is premitive datatype. it is predefined by JAVA.
+    }
+}
