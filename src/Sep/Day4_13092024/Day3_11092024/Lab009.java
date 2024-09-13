0f23ede24a65b24fb936640e56333109c0638b5f
Pawan <pawan220787@gmail.com>
2024-09-13 16:04:57 +0530

    Added final functiona program

---
+++ src/Sep/Day4_13092024/Day3_11092024/Lab009.java
@@ -0,0 +1,11 @@
+package Sep.Day4_13092024.Day3_11092024;
+
+public class Lab009 {
+    public static void main(String[] args) {
+       final String satya = "Sun rises in the east";
+        //satya = "pawan";
+        System.out.print(satya); // if final keyword is used then cant reassign any value again.
+
+
+    }
+}
