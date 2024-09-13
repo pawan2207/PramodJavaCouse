acf9acee58b4428786deae4d4ebe7b24846fc332
Pawan <pawan220787@gmail.com>
2024-09-13 15:54:57 +0530

    Added string formatting printing

---
+++ src/Sep/Day4_13092024/Day3_11092024/Lab008.java
@@ -0,0 +1,21 @@
+package Sep.Day4_13092024.Day3_11092024;
+
+public class Lab008 {
+    public static void main (String[] args){
+//string practice
+        String str1 = "My";
+        String str2 = "name is khan";
+        String str3 = "and i am";
+        String str4 = "not a terrorist";
+
+        System.out.printf("This is the movie name %s %s %s %s ", str1, str2, str3, str4+" kajol");
+
+
+
+
+    }
+
+
+
+
+}
