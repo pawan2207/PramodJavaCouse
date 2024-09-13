0cc73f336a8b939dbbbe43395723b9a66eb8a1a0
Pawan <pawan220787@gmail.com>
2024-09-13 15:43:59 +0530

    Added table of provided number

--- src/Sep/Day4_13092024/Day3_11092024/Lab007.java
+++ src/Sep/Day4_13092024/Day3_11092024/Lab007.java
@@ -6,4 +6,25 @@ public class Lab007 {
         int a = 473847;
     //int is datatype. a is variable, 473847 is value. this is premitive datatype. it is predefined by JAVA.
     }
+
+    public static class Lab006 {
+        public static void main(String[] args) {
+            //print table using %d
+            int num = 9;
+            System.out.printf("%d X %d = %d", num, 1, num * 1);
+            System.out.printf("\n");
+            System.out.printf("%d X %d = %d", num, 2, num * 2);
+            System.out.printf("\n");
+            System.out.printf("%d X %d = %d", num, 3, num * 3);
+            System.out.printf("\n");
+            System.out.printf("%d X %d = %d", num, 4, num *4);
+            System.out.printf("\n");
+            System.out.printf("%d X %d = %d", num, 5, num *5);
+            System.out.printf("\n");
+            System.out.printf("%d X %d = %d", num, 6, num *6);
+            System.out.printf("\n");
+            System.out.printf("%d X %d = %d", num, 7, num *7);
+
+        }
+    }
 }
